c8067babb307e10c36b95ab94f31314f7a477581
Mah-zaib <mahzaib067@gmail.com>
2022-12-28 03:21:06 -0800

    login signup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/Login';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Signup from './components/Signup';
 function App() {
-  console.log("hello");
+  // console.log("hello");
   return (
-    <div className="App">
-     
+    <div className="App"> 
+    <BrowserRouter>
+    <Routes>
+    <Route path='/Login' element={<Login/>} ></Route>
+    <Route path='/Signup' element={<Signup/>} ></Route>
+    </Routes>
+    </BrowserRouter> 
+     {/* <Login/> */}
+     {/* <Signup/> */}
     </div>
   );
 }
